6029553af84f3066a5837764daccf431f8639c25
= <AYOUBMAJID71@GMAIL.COM>
2024-04-28 18:53:12 +0100

    add cv  language option button  home page 2/28/24 6:53

--- JS/home/home.js
+++ JS/home/home.js
@@ -2,13 +2,24 @@
 let pMainContent = document.getElementById("mainContent");
 
 //  change the content for mobile :
-let mobileContent = `As a front-end developer, I'm passionate about creating visually stunning and user-friendly interfaces. With my knowledge of HTML, CSS, and JavaScript, I transform designs into responsive websites and applications. I keep up-to-date with industry trends and strive for seamless user experiences. I collaborate with designers and back-end developers to ensure cohesiveness and take pride in writing clean, maintainable code that adheres to best practices. `;
+let mobileContent = `
+Welcome to my portfolio! I'm a third-year computer engineering student at EMSI, passionate about front-end development. With expertise in HTML, CSS, and JavaScript, I specialize in crafting visually stunning and intuitive user interfaces. Explore my projects to see how I bring designs to life and create seamless user experiences. Let's connect and collaborate!
+`;
 
-let medLargeContent = `As a front-end developer, I'm passionate about creating visually stunning and user-friendly interfaces. With my knowledge of HTML, CSS, and JavaScript, I transform designs into responsive websites and applications. I keep up-to-date with industry trends and strive for seamless user experiences. I collaborate with designers and back-end developers to ensure cohesiveness and take pride in writing clean, maintainable code that adheres to best practices. I'm always eager to learn and
-improve my skills.`;
+let medLargeContent = `
+Hello and welcome to my portfolio! I'm currently a third-year computer engineering student at EMSI, with a background in Experimental Sciences. My journey at EMSI has deepened my understanding of mathematics, physics, and chemistry, igniting my passion for computer science and engineering. As a front-end developer, I specialize in crafting visually appealing and user-friendly interfaces using HTML, CSS, and JavaScript. I stay updated with industry trends to ensure seamless user experiences. Let's collaborate and create something amazing together!
+`
 
 if (window.innerWidth <= 768) {
 	pMainContent.innerText = mobileContent;
 } else {
 	pMainContent.innerText = medLargeContent;
+}
+let cva = document.querySelector(".Cv a");
+let selectLanguageSelect = document.querySelector(".Cv select");
+
+
+
+selectLanguageSelect.onchange=function(){	
+	cva.href= this.value=='Fr' ? "../../pdf/cv/Fr.pdf" : "../../pdf/cv/En.pdf"; 
 }
