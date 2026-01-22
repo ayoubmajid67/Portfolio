71f1c8ad51e959e9801e40787518b18528f52b78
ayoub majid <ayoubmajid71@gmail.com>
2026-01-22 10:32:34 +0100

    updated the services home stat

--- JS/home/home.js
+++ JS/home/home.js
@@ -4,7 +4,7 @@ let pMainContent = document.getElementById("mainContent");
 //  change the content for mobile :
 let mobileContent = `
 Hi! I’m Ayoub Majjid, a 5th-year computer engineering student at EMSI and currently a Tech Lead & Entrepreneur at Intellcap. I’m passionate about software development, system design, and project management. I love building scalable solutions and leading tech teams.
-I am now seeking a Final Year Project (PFE) / Pre-Hire (Pré-Embauche) opportunity in Dev devops.
+I will soon begin my Java & Microservices Internship at Capgemini TS, where I will gain hands-on experience in enterprise-grade software development, modern backend architectures, and industry best practices.
 
 `;
 
@@ -12,8 +12,7 @@ let medLargeContent = `
 Hello and welcome to my portfolio! I’m Ayoub Majjid, a fifth-year computer engineering student at EMSI, with a background in Experimental Sciences. My journey at EMSI has strengthened my foundation in mathematics, physics, and chemistry, and deepened my passion for technology and system design.
 
 Currently, I serve as Tech Lead and Entrepreneur at Intellcap, where I am leading 3 innovation projects. with the goal of transforming them into impactful and scalable startups. I enjoy guiding teams from idea to execution.
-
-I am now seeking a Final Year Project (PFE) / Pre-Hire (Pré-Embauche) opportunity in DevOps, Full Stack Development, or Software Engineering.
+I will soon begin my Java & Microservices Internship at Capgemini TS, where I will gain hands-on experience in enterprise-grade software development, modern backend architectures, and industry best practices.
 Let’s connect and create something impactful together!
 `;
 
