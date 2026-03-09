1ac3981fa728348e8aeb7d0a66e09fa3e5a37225
ayoub majid <ayoubmajid71@gmail.com>
2026-03-09 10:56:04 +0000

    added java certificate

--- JS/home/home.js
+++ JS/home/home.js
@@ -3,17 +3,17 @@ let pMainContent = document.getElementById("mainContent");
 
 //  change the content for mobile :
 let mobileContent = `
-Hi! I’m Ayoub Majjid, a 5th-year computer engineering student at EMSI and currently a Tech Lead & Entrepreneur at Intellcap. I’m passionate about software development, system design, and project management. I love building scalable solutions and leading tech teams.
-I will soon begin my Java & Microservices Internship at Capgemini TS, where I will gain hands-on experience in enterprise-grade software development, modern backend architectures, and industry best practices.
+Hi! I'm Ayoub Majjid, a 5th-year computer engineering student at EMSI. Previously a Tech Lead & Entrepreneur at Intellcap for 11 months, I'm passionate about software development, system design, and project management. I love building scalable solutions and leading tech teams.
+Currently, I'm a Java Developer Intern at Capgemini TS, gaining hands-on experience in enterprise-grade software development, modern backend architectures, and industry best practices.
 
 `;
 
 let medLargeContent = `
-Hello and welcome to my portfolio! I’m Ayoub Majjid, a fifth-year computer engineering student at EMSI, with a background in Experimental Sciences. My journey at EMSI has strengthened my foundation in mathematics, physics, and chemistry, and deepened my passion for technology and system design.
+Hello and welcome to my portfolio! I'm Ayoub Majjid, a fifth-year computer engineering student at EMSI, with a background in Experimental Sciences. My journey at EMSI has strengthened my foundation in mathematics, physics, and chemistry, and deepened my passion for technology and system design.
 
-Currently, I serve as Tech Lead and Entrepreneur at Intellcap, where I am leading 3 innovation projects. with the goal of transforming them into impactful and scalable startups. I enjoy guiding teams from idea to execution.
-I will soon begin my Java & Microservices Internship at Capgemini TS, where I will gain hands-on experience in enterprise-grade software development, modern backend architectures, and industry best practices.
-Let’s connect and create something impactful together!
+Previously, I served as Tech Lead and Entrepreneur at Intellcap for 11 months, where I led 3 innovation projects with the goal of transforming them into impactful and scalable startups. I enjoy guiding teams from idea to execution.
+Currently, I'm a Java Developer Intern at Capgemini TS, gaining hands-on experience in enterprise-grade software development, modern backend architectures, and industry best practices.
+Let's connect and create something impactful together!
 `;
 
 if (window.innerWidth <= 768) {
